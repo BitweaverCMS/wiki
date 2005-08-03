4af39a708c0f34f18b1513535d990c0fa4c26c38
Lester Caine <lester@lsces.co.uk>
2005-08-03 16:53:58 +0000

    Clears all errors reported in phpdoc build Clean build is up on http://home.lsces.co.uk/bitweaverdocsPHP/

--- modules/mod_random_pages.php
+++ modules/mod_random_pages.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_wiki/modules/mod_random_pages.php,v 1.1.1.1.2.3 2005/07/26 15:50:48 drewslater Exp $
+ * $Header: /cvsroot/bitweaver/_bit_wiki/modules/mod_random_pages.php,v 1.1.1.1.2.4 2005/08/03 16:53:58 lsces Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,9 +8,13 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: mod_random_pages.php,v 1.1.1.1.2.3 2005/07/26 15:50:48 drewslater Exp $
+ * $Id: mod_random_pages.php,v 1.1.1.1.2.4 2005/08/03 16:53:58 lsces Exp $
  * @package wiki
  * @subpackage modules
+ */
+ 
+/**
+ * Required files
  */
 require_once( WIKI_PKG_PATH.'BitPage.php' );
 global $wikilib;
