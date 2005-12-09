a31c91136ba9ba02208c5eac4450a1d85ad4ed1a
Christian Fowler <spider@viovio.com>
2005-12-09 07:11:45 +0000

    remove accidental commit of this parameter to parseData

--- BitPage.php
+++ BitPage.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_wiki/BitPage.php,v 1.2.2.35 2005/12/09 07:07:05 spiderr Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_wiki/BitPage.php,v 1.2.2.36 2005/12/09 07:11:45 spiderr Exp $
  * @package wiki
  *
  * @author spider <spider@steelsun.com>
  *
- * @version $Revision: 1.2.2.35 $ $Date: 2005/12/09 07:07:05 $ $Author: spiderr $
+ * @version $Revision: 1.2.2.36 $ $Date: 2005/12/09 07:11:45 $ $Author: spiderr $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -13,7 +13,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: BitPage.php,v 1.2.2.35 2005/12/09 07:07:05 spiderr Exp $
+ * $Id: BitPage.php,v 1.2.2.36 2005/12/09 07:11:45 spiderr Exp $
  */
 
 /**
@@ -1409,7 +1409,7 @@ class WikiLib extends BitPage {
 		while ($res = $result->fetchRow()) {
 			$title = $res["title"] . '.html';
 
-			$dat = $this->parseData($res["data"], $this);
+			$dat = $this->parseData($res["data"]);
 			// Now change index.php?page=foo to foo.html
 			// and index.php to HomePage.html
 			$dat = preg_replace("/index.php\?page=([^\'\"\$]+)/", "$1.html", $dat);
