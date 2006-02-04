96d1cd7fcd3c2be0049b9b62635f837d96ffeb9c
Stephan Borg <wolff_borg@users.sourceforge.net>
2006-02-04 03:23:19 +0000

    Fixed wrong edit field in spellcheck.

--- edit.php
+++ edit.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_wiki/edit.php,v 1.1.1.1.2.30 2006/01/28 09:19:48 squareing Exp $
+ * $Header: /cvsroot/bitweaver/_bit_wiki/edit.php,v 1.1.1.1.2.31 2006/02/04 03:23:19 wolff_borg Exp $
  *
  * Copyright( c ) 2004 bitweaver.org
  * Copyright( c ) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: edit.php,v 1.1.1.1.2.30 2006/01/28 09:19:48 squareing Exp $
+ * $Id: edit.php,v 1.1.1.1.2.31 2006/02/04 03:23:19 wolff_borg Exp $
  * @package wiki
  * @subpackage functions
  */
@@ -403,7 +403,7 @@ if( isset( $_REQUEST["preview"] ) ) {
 	//This nice function does all the job!
 	if( $gBitSystem->isFeatureActive( 'wiki_spellcheck' )) {
 		if( isset( $_REQUEST["spellcheck"] ) && $_REQUEST["spellcheck"] == 'on' ) {
-			$parsed = $gBitSystem->spellcheckreplace( $edit_data, $parsed, $gBitLanguage->mLanguage, 'editwiki' );
+			$parsed = $gBitSystem->spellcheckreplace( $formInfo['edit'], $parsed, $gBitLanguage->mLanguage, 'editwiki' );
 			$gBitSmarty->assign( 'spellcheck', 'y' );
 		} else {
 			$gBitSmarty->assign( 'spellcheck', 'n' );
