04ce6fd2958c5d87a9a09755628dbbfba4dda0d7
Christian Fowler <spider@viovio.com>
2006-03-01 19:22:57 +0000

    clean up

--- s5.php
+++ s5.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_wiki/s5.php,v 1.5 2006/02/09 14:52:47 squareing Exp $
+ * $Header: /cvsroot/bitweaver/_bit_wiki/s5.php,v 1.6 2006/03/01 19:22:57 spiderr Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,16 +8,16 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: s5.php,v 1.5 2006/02/09 14:52:47 squareing Exp $
+ * $Id: s5.php,v 1.6 2006/03/01 19:22:57 spiderr Exp $
  * @package wiki
  * @subpackage functions
  */
 
 	// we need to split the page into seperate slides
-	$slides = explode( '<h1>', $pdata );
+	$slides = explode( '<h1>', $gContent->getField('parsed_data') );
 	//vd($slides);
 	// manually set the first slide to page title and description
-	$s5 .= '<li class="slide"><h1>'.$gContent->mInfo['title'].'</h1>';
+	$s5 .= '<li class="slide"><h1>'.$gContent->getTitle().'</h1>';
 	$s5 .= '<h3>'.$gContent->mInfo['description'].'</h3></li>';
 	foreach( $slides as $slide ) {
 		if( !empty( $slide ) ) {
@@ -31,7 +31,7 @@
 	}
 	// manually set the last slide with a link back to the wiki page
 	$s5 .= '<li class="slide"><h1 style="margin:30% 0 10% 0;">'.tra( 'The End' ).'</h1>';
-	$s5 .= '<p><a href="'.WIKI_PKG_URL.'index.php?page_id='.$gContent->mInfo['page_id'].'">'.tra( 'back to the wiki page' ).'</a></p></li>';
+	$s5 .= '<p><a href="'.WIKI_PKG_URL.'index.php?page_id='.$gContent->getField('page_id').'">'.tra( 'back to the wiki page' ).'</a></p></li>';
 	$gBitSmarty->assign( 's5', $s5 );
 	$gBitSmarty->display( 'bitpackage:wiki/s5.tpl' );
 	die;
