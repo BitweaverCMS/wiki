deafcee6ede6a5c64a956c207ccdc302c46f4f06
Sylvie Greverend <sylvieg@users.sourceforge.net>
2006-06-24 22:31:03 +0000

    to have the create page link in the error if page does�'t exist

--- lookup_page_inc.php
+++ lookup_page_inc.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_wiki/lookup_page_inc.php,v 1.15 2006/04/14 19:36:19 squareing Exp $
+ * $Header: /cvsroot/bitweaver/_bit_wiki/lookup_page_inc.php,v 1.16 2006/06/24 22:31:03 sylvieg Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: lookup_page_inc.php,v 1.15 2006/04/14 19:36:19 squareing Exp $
+ * $Id: lookup_page_inc.php,v 1.16 2006/06/24 22:31:03 sylvieg Exp $
  * @package wiki
  * @subpackage functions
  */
@@ -41,6 +41,8 @@
 						$gContent->mPageId = $existsInfo[0]['page_id'];
 					}
 				}
+			} elseif ($loadPage) {
+				$gBitSmarty->assign('page', $loadPage);//to have the create page link in the error
 			}
 		}
 		if( !$gContent->load() && $loadPage ) {
