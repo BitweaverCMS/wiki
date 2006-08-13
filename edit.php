f4bd5380943eb6f3e508991dd67b1aa8516a6557
Hash9 <hash9@users.sourceforge.net>
2006-08-13 21:28:12 +0000

    Fix wiki so the selected format is shown after doing a preview

--- edit.php
+++ edit.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_wiki/edit.php,v 1.25 2006/04/29 16:01:18 squareing Exp $
+ * $Header: /cvsroot/bitweaver/_bit_wiki/edit.php,v 1.26 2006/08/13 21:28:12 hash9 Exp $
  *
  * Copyright( c ) 2004 bitweaver.org
  * Copyright( c ) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: edit.php,v 1.25 2006/04/29 16:01:18 squareing Exp $
+ * $Id: edit.php,v 1.26 2006/08/13 21:28:12 hash9 Exp $
  * @package wiki
  * @subpackage functions
  */
@@ -425,6 +425,10 @@ if( isset( $_REQUEST["fCancel"] ) ) {
 	// perhaps we have a javascript non-saving form submit
 	$formInfo = $_REQUEST;
 	$formInfo['data'] = &$_REQUEST['edit'];
+}
+
+if( isset( $_REQUEST['format_guid'] ) && !isset( $gContent->mInfo['format_guid'] ) ) {
+	$formInfo['format_guid'] = $gContent->mInfo['format_guid'] = $_REQUEST['format_guid']; 
 }
 
 if( isset( $_REQUEST["preview"] ) ) {
