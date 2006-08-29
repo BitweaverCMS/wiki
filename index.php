74159f86cc82830787f5fe8f9b0fe3bf52e86f14
Max Kremmel <xing@synapse.plus.com>
2006-08-29 13:00:26 +0000

    fix indendation

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_wiki/index.php,v 1.8 2006/07/23 00:56:06 jht001 Exp $
+ * $Header: /cvsroot/bitweaver/_bit_wiki/index.php,v 1.9 2006/08/29 13:00:26 squareing Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: index.php,v 1.8 2006/07/23 00:56:06 jht001 Exp $
+ * $Id: index.php,v 1.9 2006/08/29 13:00:26 squareing Exp $
  * @package wiki
  * @subpackage functions
  */
@@ -23,28 +23,28 @@ if (!defined('WIKI_PKG_PATH')) {
 	// need to make better than this:
 	echo "Wiki Module is not Active";
 	exit; 
-	}
+}
 require_once( WIKI_PKG_PATH.'BitPage.php' );
 
-	if( !empty( $_REQUEST['structure_id'] ) ) {
-		include( LIBERTY_PKG_PATH.'display_structure_inc.php' );
-	} else {
-		if ( !isset( $_REQUEST['page'] ) and !isset( $_REQUEST['page_id'] ) ) {
-			$_REQUEST['page'] = $gBitSystem->getConfig( 'wiki_home_page', 'HomePage' );
-		}
-		$gHome = new BitPage();
-		$wikiHome = $gBitSystem->getConfig("wiki_home_page", 'HomePage');
-		if( !($gHome->pageExists( $wikiHome )) ) {
-			$homeHash = array(
-				'title' => (isset( $wikiHome ) ? $wikiHome : 'HomePage'),
-				'creator_user_id' => ROOT_USER_ID,
-				'modifier_user_id' => ROOT_USER_ID,
-				'edit' => 'Welcome to '.( $gBitSystem->getConfig( 'site_title', 'our site' ) ) );
-			$gHome->store( $homeHash );
-		}
-
-		include( WIKI_PKG_PATH.'lookup_page_inc.php' );
-		include( WIKI_PKG_PATH.'display_bitpage_inc.php' );
+if( !empty( $_REQUEST['structure_id'] ) ) {
+	include( LIBERTY_PKG_PATH.'display_structure_inc.php' );
+} else {
+	if ( !isset( $_REQUEST['page'] ) and !isset( $_REQUEST['page_id'] ) ) {
+		$_REQUEST['page'] = $gBitSystem->getConfig( 'wiki_home_page', 'HomePage' );
 	}
+	$gHome = new BitPage();
+	$wikiHome = $gBitSystem->getConfig("wiki_home_page", 'HomePage');
+	if( !($gHome->pageExists( $wikiHome )) ) {
+		$homeHash = array(
+			'title' => (isset( $wikiHome ) ? $wikiHome : 'HomePage'),
+			'creator_user_id' => ROOT_USER_ID,
+			'modifier_user_id' => ROOT_USER_ID,
+			'edit' => 'Welcome to '.( $gBitSystem->getConfig( 'site_title', 'our site' ) ) );
+		$gHome->store( $homeHash );
+	}
+
+	include( WIKI_PKG_PATH.'lookup_page_inc.php' );
+	include( WIKI_PKG_PATH.'display_bitpage_inc.php' );
+}
 
 ?>
