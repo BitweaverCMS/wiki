bdeb1104acdd9f0c2352ac534f4b361aea287c50
laetzer <laetzer@users.sourceforge.net>
2007-05-23 05:36:58 +0000

    restored docbook comments

--- modules/mod_last_modif_pages.php
+++ modules/mod_last_modif_pages.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_wiki/modules/mod_last_modif_pages.php,v 1.6 2007/05/23 04:20:37 laetzer Exp $
+ * $Header: /cvsroot/bitweaver/_bit_wiki/modules/mod_last_modif_pages.php,v 1.7 2007/05/23 05:36:58 laetzer Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,13 +8,17 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: mod_last_modif_pages.php,v 1.6 2007/05/23 04:20:37 laetzer Exp $
+ * $Id: mod_last_modif_pages.php,v 1.7 2007/05/23 05:36:58 laetzer Exp $
  * @package wiki
  * @subpackage modules
  */
 global $gQueryUserId, $moduleParams, $wikilib;
 $params = $moduleParams['module_params'];
 
+/**
+ * required setup
+ */
+ 
 if( $gBitUser->hasPermission( 'p_wiki_view_page' ) ) {
 	require_once( WIKI_PKG_PATH.'BitPage.php' );
 	$listHash = array(
