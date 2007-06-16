f984ba4adc031f844f29c84f4cb74f05dcce2e78
Lee L Bell <starrrider@users.sourceforge.net>
2007-06-16 08:05:55 +0000

    Removed all references to obsolete variable "wiki_icache"

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -118,7 +118,6 @@ $gBitInstaller->registerPreferences( WIKI_PKG_NAME, array(
 	//array( WIKI_PKG_NAME, 'wiki_comments','n'),
 	array( WIKI_PKG_NAME, 'wiki_description','y'),
 	//array( WIKI_PKG_NAME, 'wiki_footnotes','n'),
-	//array( WIKI_PKG_NAME, 'wiki_icache','n'),
 	//array( WIKI_PKG_NAME, 'wiki_monosp','n'),
 	//array( WIKI_PKG_NAME, 'wiki_multiprint','n'),
 	//array( WIKI_PKG_NAME, 'wiki_notepad','n'),
