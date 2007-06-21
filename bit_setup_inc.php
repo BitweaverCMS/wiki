80d72924b7a7ac55a179960df5e5d4157206a5bf
Max Kremmel <xing@synapse.plus.com>
2007-06-21 09:49:15 +0000

    provide an easy means (admin_comments_url) for packages to indicate where in the admin interface you can modify comment settings

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -13,9 +13,10 @@ define('BITPAGE_CONTENT_TYPE_GUID', 'bitpage' );
 if($gBitSystem->isPackageActive( 'wiki' ) ) {
 	if ($gBitUser->hasPermission( 'p_wiki_view_page' )) {
 		$menuHash = array(
-			'package_name'  => WIKI_PKG_NAME,
-			'index_url'     => WIKI_PKG_URL.'index.php',
-			'menu_template' => 'bitpackage:wiki/menu_wiki.tpl',
+			'package_name'       => WIKI_PKG_NAME,
+			'index_url'          => WIKI_PKG_URL.'index.php',
+			'menu_template'      => 'bitpackage:wiki/menu_wiki.tpl',
+			'admin_comments_url' => KERNEL_PKG_URL.'admin/index.php?page=wiki',
 		);
 		$gBitSystem->registerAppMenu( $menuHash );
 	}
