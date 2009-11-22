ddb1565ce6548d8b8f04a0e1b67279196ae65cfa
Lester Caine <lester@lsces.co.uk>
2009-11-22 11:02:26 +0000

    Allow ordering of backlinks - alternative may be ORDER BY title

--- BitPage.php
+++ BitPage.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_wiki/BitPage.php,v 1.122 2009/10/01 14:17:07 wjames5 Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_wiki/BitPage.php,v 1.123 2009/11/22 11:02:26 lsces Exp $
  * @package wiki
  *
  * @author spider <spider@steelsun.com>
  *
- * @version $Revision: 1.122 $ $Date: 2009/10/01 14:17:07 $ $Author: wjames5 $
+ * @version $Revision: 1.123 $ $Date: 2009/11/22 11:02:26 $ $Author: lsces $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -13,7 +13,7 @@
  * All Rights Reserved. See below for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details
  *
- * $Id: BitPage.php,v 1.122 2009/10/01 14:17:07 wjames5 Exp $
+ * $Id: BitPage.php,v 1.123 2009/11/22 11:02:26 lsces Exp $
  */
 
 /**
@@ -491,7 +491,7 @@ class BitPage extends LibertyMime {
 			$to_title = $this->mInfo['title'];
 			$query = "SELECT lcl.`from_content_id`, lc.`title`
 					  FROM `".BIT_DB_PREFIX."liberty_content_links` lcl INNER JOIN `".BIT_DB_PREFIX."liberty_content` lc ON (lcl.`from_content_id`=lc.`content_id`)
-					  WHERE lcl.`to_title` = ?";
+					  WHERE lcl.`to_title` = ? ORDER BY lcl.POS";
 			$ret = $this->mDb->getAssoc( $query, array( $to_title ) );
 			return $ret;
 		}
