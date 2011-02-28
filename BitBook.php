9cfd032dfb6ceaff55473484775f860530df082a
spiderr <spider@viovio.com>
2011-02-27 23:42:51 -0500

    mViewContentPerm is the same as the base class, p_wiki_view_page

--- BitBook.php
+++ BitBook.php
@@ -53,7 +53,6 @@ class BitBook extends BitPage {
 		$this->mContentTypeGuid = BITBOOK_CONTENT_TYPE_GUID;
 
 		// Permission setup
-		$this->mViewContentPerm  = 'p_wiki_view_page';
 		$this->mCreateContentPerm  = 'p_wiki_create_book';
 		$this->mUpdateContentPerm  = 'p_wiki_update_book';
 		$this->mAdminContentPerm = 'p_wiki_admin_book';
