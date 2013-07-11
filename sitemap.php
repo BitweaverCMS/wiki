f122c080540bcc3b41e7d2656c88ed112bd3f48f
lsces <lester@lsces.co.uk>
2013-07-11 22:27:49 +0100

    Add docblock header

--- sitemap.php
+++ sitemap.php
@@ -1,5 +1,18 @@
 <?php
+/**
+ * Copyright (c) 2004 bitweaver.org
+ * Copyright (c) 2003 tikwiki.org
+ * Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+ * All Rights Reserved. See below for details and a complete list of authors.
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details
+ *
+ * @package wiki
+ * @subpackage functions
+ */
 
+/**
+ * required setup
+ */
 require_once( '../kernel/setup_inc.php' );
 
 require_once( WIKI_PKG_PATH.'BitBook.php' );
